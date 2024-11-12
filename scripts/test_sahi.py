7354fb91311da2c9feeae1d44c70da4e9ae63316
Bohyung Kim <giantbalance@gmail.com>
2024-11-12 22:03:05 +0900

    Bbox center (#2)
    * Center Bbox 를 출력하는 옵션 추가

--- scripts/test_sahi.py
+++ scripts/test_sahi.py
@@ -83,6 +83,13 @@ CATEGORIES = [
     }
 ]
 
+def center_bbox(bbox):
+    x, y, w, h = bbox
+    return {
+        "x": x + w / 2,
+        "y": y + h / 2,
+    }
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -92,6 +99,7 @@ def parse_args():
     parser.add_argument("--out-dir", type=str, default="out_dirs")
     parser.add_argument("--score-thr", type=float, default=0.3)
     parser.add_argument("--export-vis", action="store_true", default=False)
+    parser.add_argument("--center-bbox", action="store_true", default=False)
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -141,6 +149,9 @@ if __name__ == "__main__":
             } for pred in preds],
         }
 
+        if args.center_bbox:
+            preds_dict["center"] = [center_bbox(bbox) for bbox in preds_dict["bboxes"]]
+            
         output_path = os.path.join(output_pred_dir, fn.replace(Path(fn).suffix, ".json"))
         with open(output_path, "w") as f:
             json.dump(preds_dict, f, indent=4, ensure_ascii=False)
